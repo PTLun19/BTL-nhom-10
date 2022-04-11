845c9963b791dc3a807b806ce69416ed24d7310f
Vananh02 <2051062391@e.tlu.edu.vn>
2022-04-11 23:27:53 +0700

    Vanh day bai

--- script.js
+++ script.js
@@ -2,16 +2,25 @@ let name = document.getElementById('contact-name')
 let statusOfName = document.getElementById('form-validation1')
 name.addEventListener('focusout', leaveName)
 function leaveName(){
-  if(name.value ==''){
+  if(name.value == ''){
     statusOfName.textContent = 'The text field is requied'
+  }else{
+    statusOfName.textContent = undefined
   }
 }
 let phone = document.getElementById('contact-phone')
 let statusOfPhone = document.getElementById('form-validation2')
 phone.addEventListener('focusout', leavePhone)
 function leavePhone(){
-  if(phone.value ==''){
+  if(phone.value ==""){
     statusOfPhone.textContent = 'The text field is requied'
+  }else{
+    var PhoneRegex = /^[0-9]+$/
+    if(PhoneRegex.test(phone.value)){
+     statusOfPhone.textContent = undefined
+    }else{
+     statusOfPhone.textContent = 'Only numbers are required'
+    }
   }
 }
 let email = document.getElementById('contact-email')
@@ -20,13 +29,22 @@ email.addEventListener('focusout', leaveMail)
 function leaveMail(){
   if(email.value ==''){
     statusOfEmail.textContent = 'The email is requied'
+  }else{
+    var emailRegex = /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/
+    if(emailRegex.test(email.value)){
+     statusOfEmail.textContent = undefined
+    }else{
+     statusOfEmail.textContent = 'The email is not a valid email'
+    }
   }
 }
 let message = document.getElementById('contact-message')
-let statusOfMesage = document.getElementById('form-validation4')
-massage.addEventListener('focusout', leaveMessage)
+let statusOfMessage = document.getElementById('form-validation4')
+message.addEventListener('focusout', leaveMessage)
 function leaveMessage(){
   if(message.value ==''){
     statusOfMessage.textContent = 'The text area is requied'
+  }else{
+    statusOfMessage.textContent = undefined
   }
 }
